c089a1808437f93d57de8ff2554861030dd8cb8b
Emiliano Alves <emilianod3@gmail.com>
2026-06-12 11:05:17 -0300

    .env

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'number' => '1.0.11',
-    'hash' => 'c2b055e',
-    'timestamp' => '2026-06-12 15:58:58',
+    'number' => '1.0.12',
+    'hash' => 'd2f561c',
+    'timestamp' => '2026-06-12 16:05:19',
 ];
